8a9fca1c4f46f3dd4f46c9ef5f09c45e54f9e337
Matthijs <m.zwaan@student.fontys.nl>
2025-01-07 13:08:57 +0100

    Mock DAL implementation

---
+++ DAL/Mock/TestRepositoryMock.cs
@@ -0,0 +1,45 @@
+﻿using BLL.Agents;
+using BLL.Tests;
+using BLL.Customers;
+using System;
+
+namespace DAL.Mock
+{
+    public class TestRepositoryMock : ITestRepository
+    {
+        private List<Test> _Tests = new List<Test>();
+
+        public IReadOnlyList<Test> Tests { get { return _Tests.AsReadOnly(); } }
+
+        public TestRepositoryMock() 
+        {
+            _Tests.Add(new Test("CheckStatusPage", CustomerStatus.Error, new List<string> { "url" }));
+            _Tests.Add(new Test("CheckService", CustomerStatus.Error, new List<string> { "service" }));
+        }
+
+
+        public bool Create(Test test)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Test test)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Test Retrieve(string testType)
+        {
+            foreach (Test test in _Tests)
+            {
+                if (test.TestType == testType) return test;
+            }
+            return null;
+        }
+
+        public bool Update(Test test)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
