1e119ff2a92c422185f52b8c042d0e6c6ef35910
Matthijs <147452309+m-zwaan@users.noreply.github.com>
2025-01-16 17:08:30 +0100

    Updated script to start website

--- .github/workflows/start_website.ps1
+++ .github/workflows/start_website.ps1
@@ -1,10 +1,16 @@
-if ((Get-WebSiteState -Name FT-API).Value -eq "Stopped")
-{
+Import-Module WebAdministration
+echo "Starting website"
+if ((Get-WebSiteState -Name FT-API).Value -eq "Stopped"){
     Start-WebSite -Name FT-API
     echo "Started Website FT-API"
+} else {
+    echo "Website FT-API already started"
 }
-if ((Get-WebAppPoolState -Name FT-API).Value -eq "Stopped")
-{
+if ((Get-WebAppPoolState -Name FT-API).Value -eq "Stopped") {
     Start-WebAppPool -Name FT-API
     echo "Started Application Pool FT"
+} else {
+    echo "Application pool FT-API already started"
 }
+echo "Website started."
+Remove-Module WebAdministration
