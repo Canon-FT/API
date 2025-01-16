3f0eb27e0c3b2da34165c59a5ffc1d59650b2df7
Matthijs <147452309+m-zwaan@users.noreply.github.com>
2025-01-16 17:09:51 +0100

    Changed logic in script
    Instead of equals "started", it looks for not "stopped"

--- .github/workflows/stop_website.ps1
+++ .github/workflows/stop_website.ps1
@@ -1,12 +1,12 @@
 Import-Module WebAdministration
 echo "Stopping website"
-if ((Get-WebSiteState -Name FT-API).Value -eq "Started") {
+if ((Get-WebSiteState -Name FT-API).Value -ne "Stopped") {
     Stop-WebSite -Name FT-API
     echo "Stopped Website FT-API"
 } else {
     echo "Website FT-API already stopped"
 }
-if ((Get-WebAppPoolState -Name FT-API).Value -eq "Started") {
+if ((Get-WebAppPoolState -Name FT-API).Value -ne "Stopped") {
     Stop-WebAppPool -Name FT-API
     echo "Stopped Application Pool FT-API"
 } else {
